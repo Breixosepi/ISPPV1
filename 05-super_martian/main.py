3ffc1c85192f6a847d36d64194eb3c71d561f4bd
Alejandro <aledrums@gmail.com>
2026-08-15 16:28:36 -0400

    Removed unnecessary mixer initialization and comments

--- 05-super_martian/main.py
+++ 05-super_martian/main.py
@@ -11,8 +11,5 @@ This file contains the main program to run the game.
 from src.SuperMartian import SuperMartian
 
 if __name__ == "__main__":
-    # SuperMartian takes every gale.game.Game argument (title, window
-    # size, ...) straight from settings.py / gale.conf.global_settings,
-    # so there's no need to pass any of them here -- see settings.py.
     super_martian = SuperMartian()
     super_martian.exec()
--- 07-ultimate_fantasy/main.py
+++ 07-ultimate_fantasy/main.py
@@ -15,10 +15,5 @@ from src.UltimateFantasy import UltimateFantasy
 
 if __name__ == "__main__":
     set_default_theme(DEFAULT_THEME)
-
-    # UltimateFantasy takes every gale.game.Game argument (title,
-    # window size, ...) straight from settings.py /
-    # gale.conf.global_settings, so there's no need to pass any of
-    # them here -- see settings.py.
     game = UltimateFantasy()
     game.exec()
