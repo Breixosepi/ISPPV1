d07652f6b492985dc3b5b13dea2416047bef142e
Breixosepi <125312916+Breixosepi@users.noreply.github.com>
2026-08-28 12:26:00 -0400

    implement HardLogPair with full gap closure and bump sound

--- 02-flappy_bird/src/HardLogPair.py
+++ 02-flappy_bird/src/HardLogPair.py
@@ -8,10 +8,10 @@ class HardLogPair(LogPair):
     def __init__(self, x: float, y: float, initial_gap : float =90.0) -> None:
         super().__init__(x, y)
         self.gap = initial_gap
-        self.is_dynamic = random.choice([True, False])
+        self.is_dynamic = random.random() < 0.7
         if self.is_dynamic:
-            self.vy: float = random.choice([-40, 40])  
-            self.gap_vy: float = random.choice([-20, 20])
+            self.vy: float = random.choice([-50, 50])  
+            self.gap_vy: float = random.choice([-50, 50])
         else:
             self.vy: float = 0.0
             self.gap_vy: float = 0.0
@@ -22,7 +22,7 @@ class HardLogPair(LogPair):
 
         if not self.is_dynamic:
             return
-        
+            
         self.y += self.vy * dt
         min_y = -settings.LOG_HEIGHT + 20
         max_y = (settings.VIRTUAL_HEIGHT - settings.GROUND_HEIGHT - self.gap - settings.LOG_HEIGHT - 20)
@@ -34,13 +34,16 @@ class HardLogPair(LogPair):
             self.y = max_y
             self.vy *= -1
 
+
         self.gap += self.gap_vy * dt
-        min_gap = settings.BIRD_HEIGHT * 2
+        min_gap = 0.0
         max_gap = settings.BIRD_HEIGHT * 4
 
-        if self.gap < min_gap:
+        if self.gap <= min_gap:
             self.gap = min_gap
             self.gap_vy *= -1
+            settings.SOUNDS["log_bump"].play()
+
         elif self.gap > max_gap:
             self.gap = max_gap
             self.gap_vy *= -1
